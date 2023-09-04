a2859de4691f06eb147eb260ac821947ed66f55d
anti-15 <taiki1151001@icloud.com>
2023-09-04 17:45:48 +0900

    first commit

---
+++ db/init/002-insert-test-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (email, password) VALUES
+('testuser1@email.com', 'hashed_password1'),
+('testuser2@email.com', 'hashed_password2');
+
+INSERT INTO todos (title,description) VALUES
+('test1', 'test1のdescription'),
+('test2', 'test2のdescription');
