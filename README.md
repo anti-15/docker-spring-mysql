d5f1c6e1b2ce2090c39efb63086ca699558b6e3e
anti-15 <taiki1151001@icloud.com>
2024-01-11 21:11:08 +0900

    データ変更

---
+++ README.md
@@ -0,0 +1,6 @@
+1. `docker compose up -d`
+2. `docker-compose exec mysql mysql -uroot -p`
+3. `spring_root`を入力
+4. `USE spring_database;`
+5. `SHOW TABLES;` テーブルを確認する
+6. `SELECT * FROM テーブル名;` データが挿入されているか確認する
--- db/init/002-insert-test-data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users (email, password) VALUES
-('testuser1@email.com', 'password1'),
-('testuser2@email.com', 'password2');
-
-INSERT INTO todos (title,description) VALUES
-('test1', 'test1のdescription'),
-('test2', 'test2のdescription');
